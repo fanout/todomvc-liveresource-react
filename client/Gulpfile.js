48b721d6f15675117b953ca2e8ea83c6fed354a9
Katsuyuki Ohmuro <harmony7@pex2.jp>
2015-11-03 17:39:03 +0900

    Cleaned gulpfile, all files are treated as ES6

--- client/Gulpfile.js
+++ client/Gulpfile.js
@@ -1,20 +1,17 @@
 var gulp = require("gulp");
-var gulpif = require("gulp-if");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
 
 gulp.task("default", function () {
     return gulp.src("js/src/*.+(js|jsx)")
-        //.pipe(sourcemaps.init())
-        .pipe(gulpif(/\.(jsx|es6)/, babel({
+        .pipe(babel({
             compact: false,
             presets: [
                 "react",
                 "es2015"
             ]
-        })))
+        }))
         .pipe(concat("bundle.js"))
-        //.pipe(sourcemaps.write("."))
         .pipe(gulp.dest("js/build"));
 });
