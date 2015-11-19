43886cb92498013aaf2f1bf8f5b2957ea621033d
Justin Karneges <justin@affinix.com>
2015-11-19 12:52:35 -0800

    remove warning

--- server/wsgi.py
+++ server/wsgi.py
@@ -11,7 +11,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
 vars = (
 	'DJANGO_SECRET_KEY',
