272aafecc1b470be9d57c90be288b44d8a2dbbd3
Justin Karneges <justin@affinix.com>
2016-02-16 18:01:20 -0800

    remove cling

--- server/wsgi.py
+++ server/wsgi.py
@@ -16,4 +16,4 @@ dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.en
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
