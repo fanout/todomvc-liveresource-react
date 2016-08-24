2450ff123ec888007cd3932ca7be944424c68c61
Katsuyuki Ohmuro <harmony7@pex2.jp>
2016-08-24 09:29:19 +0900

    styling info area

--- client/js/src/components/LiveResourceInfo.js
+++ client/js/src/components/LiveResourceInfo.js
@@ -4,12 +4,14 @@ class LiveResourceInfo extends React.PureComponent {
     render() {
         const { endpointUrl } = this.props;
 
+        const codeClassName = "ba b--gray br0_25 bg-ghostwhite ph0_5 pv0_25";
+
         return (
-            <div className="f4">
+            <div className="f4 mh--5">
                 <p>API docs: <a href="http://docs.realtimetodolist.apiary.io/">http://docs.realtimetodolist.apiary.io/</a></p>
                 {endpointUrl != null ? (
                     <p>
-                        Stream: <code className="example-code">curl -i <span className="endpoint">{endpointUrl}</span>?stream=true</code>
+                        Stream: <code className={codeClassName}>curl -i {endpointUrl}?stream=true</code>
                     </p>
                 ) : null}
             </div>
