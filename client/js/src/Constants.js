5f1f2e48f8cf19ba5f233776eb9f01912d9ffdfb
Justin Karneges <justin@karneges.com>
2016-08-31 12:56:34 -0700

    update api base to point to default list

--- client/js/src/Constants.js
+++ client/js/src/Constants.js
@@ -3,7 +3,7 @@ export const KeyCodes = {
     ESCAPE_KEY: 27
 };
 
-export const API_ENDPOINT = "http://todo-api.liveresource.org/todos/";
+export const API_ENDPOINT = "http://todo-api.liveresource.org/todos/default/items/";
 
 export const TodoAppModes = {
     ALL: 'all',
